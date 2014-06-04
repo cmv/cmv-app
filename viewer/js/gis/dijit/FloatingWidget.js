b44a27b20b62dcf54775ea8e3097401dd2de86e4
David Spriggs <spriggs.d@gmail.com>
2014-06-03 22:14:17 -0500

    Fixed #64

--- viewer/js/gis/dijit/FloatingWidget.js
+++ viewer/js/gis/dijit/FloatingWidget.js
@@ -3,12 +3,13 @@ define([
 	'dijit/Dialog'
 ], function(declare, Dialog) {
 	return declare([Dialog], {
-		declaredClass:'gis.dijit.FloatingWidget',
+		declaredClass: 'gis.dijit.FloatingWidget',
 		title: 'Floating Widget',
 		draggable: true,
 		'class': 'floatingWidget',
 		close: function() {
 			this.hide();
-		}
+		},
+		focus: function() {}
 	});
 });
