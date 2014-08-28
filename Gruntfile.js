aa0ddab8a739a45484976445010501bf2bf98cdf
David Spriggs <spriggs.d@gmail.com>
2014-08-27 23:19:48 -0500

    Updated grunt to run jshint on default.

--- Gruntfile.js
+++ Gruntfile.js
@@ -126,7 +126,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compress');
 
   // define the tasks
-  grunt.registerTask('default', 'Watches the project for changes, automatically builds them and runs a web server and opens default browser to preview.', ['connect:dev', 'open:dev_browser', 'watch:dev']);
+  grunt.registerTask('default', 'Watches the project for changes, automatically builds them and runs a web server and opens default browser to preview.', ['jshint', 'connect:dev', 'open:dev_browser', 'watch:dev']);
   grunt.registerTask('build', 'Compiles all of the assets and copies the files to the build directory.', ['clean', 'copy', 'scripts', 'stylesheets', 'compress:build']);
   grunt.registerTask('build-view', 'Compiles all of the assets and copies the files to the build directory starts a web server and opens browser to preview app.', ['clean', 'copy', 'scripts', 'stylesheets', 'compress:build', 'connect:build', 'open:build_browser', 'watch:build']);
   grunt.registerTask('scripts', 'Compiles the JavaScript files.', ['jshint', 'uglify']);
