270cd5e88e38c61329693e4e89fd58d5cf7d5dab
Jade Freeman <freema23@msu.edu>
2014-11-13 23:05:46 -0500

    nls and fa icons

---
+++ viewer/js/gis/dijit/Find/nls/resource.js
@@ -0,0 +1,25 @@
+// http://dojotoolkit.org/reference-guide/1.10/dojo/i18n.html
+define({
+   root: {
+       select_query: 'Select query',
+       search_text: {
+           label: 'Search for',
+           placeholder: 'Enter the text you want to search for.'
+       },
+       exact_matches: 'Find exact matches only',
+       search_button: {
+           label: 'Search',
+           busy_label: 'searching'
+       },
+       clear_button: {
+           label: 'Clear'
+       },
+       searching: 'Searching...',
+       results_label: {
+           multiple_suffix: 's',
+           label_prefix: 'Result',
+           label_suffix: 'found'
+       }
+   }
+});
+
