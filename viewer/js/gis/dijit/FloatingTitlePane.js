f7387de4b36b3f4da72df4ecc03b9a28e22dd65d
Tim McGee <github@mmconsult.com>
2014-12-16 17:00:27 -0800

    fixed formatting

--- viewer/js/gis/dijit/FloatingTitlePane.js
+++ viewer/js/gis/dijit/FloatingTitlePane.js
@@ -1,44 +1,44 @@
 define([
-	'dojo/_base/declare',
-	'dijit/TitlePane',
-	'dojo/on',
-	'dojo/_base/lang',
-	'dojo/dnd/Moveable',
-	'dojo/aspect',
-	'dojo/topic',
-	'dojo/_base/window',
-	'dojo/window',
-	'dojo/dom-geometry',
-	'dojo/dom-style',
-	'dojo/dom-construct',
-	'dojo/dom-attr',
-	'dojo/dom-class',
-	'xstyle/css!./FloatingTitlePane/css/FloatingTitlePane.css'
+    'dojo/_base/declare',
+    'dijit/TitlePane',
+    'dojo/on',
+    'dojo/_base/lang',
+    'dojo/dnd/Moveable',
+    'dojo/aspect',
+    'dojo/topic',
+    'dojo/_base/window',
+    'dojo/window',
+    'dojo/dom-geometry',
+    'dojo/dom-style',
+    'dojo/dom-construct',
+    'dojo/dom-attr',
+    'dojo/dom-class',
+    'xstyle/css!./FloatingTitlePane/css/FloatingTitlePane.css'
 ], function (declare, TitlePane, on, lang, Moveable, aspect, topic, win, winUtils, domGeom, domStyle, domConstruct, domAttr, domClass) {
-	return declare([TitlePane], {
+    return declare([TitlePane], {
         sidebarPosition: null,
-		postCreate: function () {
-			if (this.canFloat) {
-				this.createDomNodes();
-				this.own(on(window, 'resize', lang.hitch(this, '_endDrag')));
-			}
+        postCreate: function () {
+            if (this.canFloat) {
+                this.createDomNodes();
+                this.own(on(window, 'resize', lang.hitch(this, '_endDrag')));
+            }
             this.own(topic.subscribe('titlePane/event', lang.hitch(this, '_updateWidgetSidebarPosition')));
-			this.own(aspect.after(this, 'toggle', lang.hitch(this, '_afterToggle')));
-			this.inherited(arguments);
-		},
-		startup: function () {
-			if (this.titleBarNode && this.canFloat) {
-				this._moveable = new Moveable(this.domNode, {
+            this.own(aspect.after(this, 'toggle', lang.hitch(this, '_afterToggle')));
+            this.inherited(arguments);
+        },
+        startup: function () {
+            if (this.titleBarNode && this.canFloat) {
+                this._moveable = new Moveable(this.domNode, {
                     delay: 5,
-					handle: this.titleBarNode
-				});
+                    handle: this.titleBarNode
+                });
                 aspect.after(this._moveable, 'onMove', lang.hitch(this, '_dragging'), true);
-				aspect.after(this._moveable, 'onMoveStop', lang.hitch(this, '_endDrag'), true);
+                aspect.after(this._moveable, 'onMoveStop', lang.hitch(this, '_endDrag'), true);
                 aspect.after(this._moveable, 'onMoveStart', lang.hitch(this, '_startDrag'), true);
-			}
-			this.inherited(arguments);
-		},
-		createDomNodes: function () {
+            }
+            this.inherited(arguments);
+        },
+        createDomNodes: function () {
             this.moveHandleNode = domConstruct.create('span', {
                 title: 'Move widget',
                 'class': 'floatingWidgetPopout'
@@ -53,14 +53,14 @@ define([
                 this._dockWidget();
                 evt.stopImmediatePropagation();
             })));
-		},
+        },
         toggle: function () {
             if (this.isFloating && this.isDragging) {
                 return;
             }
             this.inherited(arguments);
         },
-		_dockWidget: function () {
+        _dockWidget: function () {
             if (!this.isDragging) {
                 domAttr.remove(this.domNode, 'style');
                 domStyle.set(this.dockHandleNode, 'display', 'none');
@@ -73,7 +73,7 @@ define([
                 this.isFloating = false;
                 this._updateTopic('dock');
             }
-		},
+        },
         _dragging: function () {
             this.isDragging = true;
         },
@@ -84,7 +84,7 @@ define([
             domStyle.set(this.titleBarNode, 'cursor', 'move');
 
             if (!this.isFloating) {
-            	this._checkSidebarPosition();
+                this._checkSidebarPosition();
                 domStyle.set(this.dockHandleNode, 'display', 'inline');
                 domStyle.set(this.moveHandleNode, 'display', 'none');
                 domStyle.set(this.domNode, 'z-index', '40');
@@ -102,42 +102,42 @@ define([
                 this._updateTopic('undock');
             }
         },
-		_endDrag: function () {
-			// summary:
-			//		Called after dragging the Dialog. Saves the position of the dialog in the viewport,
-			//		and also adjust position to be fully within the viewport, so user doesn't lose access to handle
-			var nodePosition = domGeom.position(this.domNode);
-			var viewport = winUtils.getBox(this.ownerDocument);
-			nodePosition.y = Math.min(Math.max(nodePosition.y, 0), (viewport.h - nodePosition.h));
-			nodePosition.x = Math.min(Math.max(nodePosition.x, 0), (viewport.w - nodePosition.w));
-			this._relativePosition = nodePosition;
-			this._position();
+        _endDrag: function () {
+            // summary:
+            //      Called after dragging the Dialog. Saves the position of the dialog in the viewport,
+            //      and also adjust position to be fully within the viewport, so user doesn't lose access to handle
+            var nodePosition = domGeom.position(this.domNode);
+            var viewport = winUtils.getBox(this.ownerDocument);
+            nodePosition.y = Math.min(Math.max(nodePosition.y, 0), (viewport.h - nodePosition.h));
+            nodePosition.x = Math.min(Math.max(nodePosition.x, 0), (viewport.w - nodePosition.w));
+            this._relativePosition = nodePosition;
+            this._position();
             domStyle.set(this.titleBarNode, 'cursor', this.titleCursor);
 
             //delayed slightly so the titlebar does not toggle
             window.setTimeout(lang.hitch(this, function () {
                 this.isDragging = false;
             }), 50);
-		},
-		_position: function () {
-			// summary:
-			//		Position the dialog in the viewport.  If no relative offset
-			//		in the viewport has been determined (by dragging, for instance),
-			//		center the dialog.  Otherwise, use the Dialog's stored relative offset,
-			//		adjusted by the viewport's scroll.
-			if (!domClass.contains(this.ownerDocumentBody, 'dojoMove')) { // don't do anything if called during auto-scroll
-				var node = this.domNode,
-					viewport = winUtils.getBox(this.ownerDocument),
-					p = this._relativePosition,
-					bb = p ? null : domGeom.position(node),
-					l = Math.floor(viewport.l + (p ? p.x : (viewport.w - bb.w) / 2)),
-					t = Math.floor(viewport.t + (p ? p.y : (viewport.h - bb.h) / 2));
-				domStyle.set(node, {
-					left: l + 'px',
-					top: t + 'px'
-				});
-			}
-		},
+        },
+        _position: function () {
+            // summary:
+            //      Position the dialog in the viewport.  If no relative offset
+            //      in the viewport has been determined (by dragging, for instance),
+            //      center the dialog.  Otherwise, use the Dialog's stored relative offset,
+            //      adjusted by the viewport's scroll.
+            if (!domClass.contains(this.ownerDocumentBody, 'dojoMove')) { // don't do anything if called during auto-scroll
+                var node = this.domNode,
+                    viewport = winUtils.getBox(this.ownerDocument),
+                    p = this._relativePosition,
+                    bb = p ? null : domGeom.position(node),
+                    l = Math.floor(viewport.l + (p ? p.x : (viewport.w - bb.w) / 2)),
+                    t = Math.floor(viewport.t + (p ? p.y : (viewport.h - bb.h) / 2));
+                domStyle.set(node, {
+                    left: l + 'px',
+                    top: t + 'px'
+                });
+            }
+        },
         _updateWidgetSidebarPosition: function (msg) {
             var id = msg.widgetID, pos = msg.sidebarPosition, action = msg.action;
 
@@ -148,7 +148,7 @@ define([
                 return;
             }
 
-        	this._checkSidebarPosition();
+            this._checkSidebarPosition();
 
             // increment the position if the other widget is docked
             // above this widget's position
@@ -174,14 +174,13 @@ define([
             if (this.sidebarPosition === null) {
                 var k = 0, len = dockedWidgets.length;
                 for (k = 0; k < len; k++) {
-                	var widget = dockedWidgets[k];
+                    var widget = dockedWidgets[k];
                     if (widget.canFloat) {
-                    	widget.sidebarPosition = k;
+                        widget.sidebarPosition = k;
                     }
                 }
             }
         },
-
         _updateTopic: function (msg) {
             topic.publish('titlePane/event', {
                 category: 'Titlepane Event',
@@ -192,9 +191,9 @@ define([
                 value: msg
             });
         },
-		_afterToggle: function () {
-			var evt = this.open ? 'open' : 'close';
-			this._updateTopic(evt);
-		}
-	});
+        _afterToggle: function () {
+            var evt = this.open ? 'open' : 'close';
+            this._updateTopic(evt);
+        }
+    });
 });
