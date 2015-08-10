12c8363bab2d2c5b880697dceddea3ab573ae3b7
Vollan Borg <vollanborg@hotmail.com>
2015-08-10 16:15:58 +0200

    Add Search widget

---
+++ viewer/js/widgets/Search/nls/Search.js
@@ -0,0 +1,26 @@
+// http://dojotoolkit.org/reference-guide/1.10/dojo/i18n.html
+define({
+    root: {
+        Labels: {
+            selectALayer: 'Select A Layer',
+            selectAQuery: 'Select A Query',
+            buffer: 'Buffer',
+            displayBuffer: 'Display Buffer Only',
+            selectFeaturesBy: 'Select Features By',
+            tabTitleByAttribute: 'By Attribute',
+            tabTitleByShape: 'By Shape'
+        },
+        Buttons: {
+            search: 'Search',
+            selectByRectangle: 'Select By Rectangle',
+            selectByCircle: 'Select By Circle',
+            selectByPoint: 'Select By Point',
+            selectByPolyline: 'Select By Line',
+            selectByFreehandPolyline: 'Select By Freehand Line',
+            selectByPolygon: 'Select By Polygon',
+            selectByFreehandPolygon: 'Select By Freehand Polygon',
+            selectByIdentify: 'Select By Identified Feature(s)',
+            selectBySelected: 'Select By Selected Feature(s)'
+        }
+    }
+});
