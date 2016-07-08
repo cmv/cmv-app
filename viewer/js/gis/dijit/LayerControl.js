a3f09e355314f509f385625915b380799f71e9ae
Tim McGee <github@mmconsult.com>
2016-07-08 12:15:29 -0700

    ensure that `_reorderUp` and `_reoderDown` dom nodes exist before calling set methods.

--- viewer/js/gis/dijit/LayerControl.js
+++ viewer/js/gis/dijit/LayerControl.js
@@ -346,29 +346,37 @@ define([
             if (this.separated) {
                 if (this.vectorReorder) {
                     array.forEach(this._vectorContainer.getChildren(), function (child) {
-                        if (!child.getPreviousSibling()) {
-                            child._reorderUp.set('disabled', true);
-                        } else {
-                            child._reorderUp.set('disabled', false);
+                        if (child._reorderUp) {
+                            if (!child.getPreviousSibling()) {
+                                child._reorderUp.set('disabled', true);
+                            } else {
+                                child._reorderUp.set('disabled', false);
+                            }
                         }
-                        if (!child.getNextSibling()) {
-                            child._reorderDown.set('disabled', true);
-                        } else {
-                            child._reorderDown.set('disabled', false);
+                        if (child._reorderDown) {
+                            if (!child.getNextSibling()) {
+                                child._reorderDown.set('disabled', true);
+                            } else {
+                                child._reorderDown.set('disabled', false);
+                            }
                         }
                     }, this);
                 }
                 if (this.overlayReorder) {
                     array.forEach(this._overlayContainer.getChildren(), function (child) {
-                        if (!child.getPreviousSibling()) {
-                            child._reorderUp.set('disabled', true);
-                        } else {
-                            child._reorderUp.set('disabled', false);
+                        if (child._reorderUp) {
+                            if (!child.getPreviousSibling()) {
+                                child._reorderUp.set('disabled', true);
+                            } else {
+                                child._reorderUp.set('disabled', false);
+                            }
                         }
-                        if (!child.getNextSibling()) {
-                            child._reorderDown.set('disabled', true);
-                        } else {
-                            child._reorderDown.set('disabled', false);
+                        if (child._reorderDown) {
+                            if (!child.getNextSibling()) {
+                                child._reorderDown.set('disabled', true);
+                            } else {
+                                child._reorderDown.set('disabled', false);
+                            }
                         }
                     }, this);
                 }
