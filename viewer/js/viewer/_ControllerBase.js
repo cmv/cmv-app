0ffeae647b2837c85b1d996308a6f25e0dc79fa9
Tim McGee <github@mmconsult.com>
2016-11-01 12:15:05 -0700

    Add support for webmaps

--- viewer/js/viewer/_ControllerBase.js
+++ viewer/js/viewer/_ControllerBase.js
@@ -32,6 +32,26 @@ define([
                 // add growler here?
                 return;
             }
+        },
+
+        mixinDeep: function (dest, source) {
+            //Recursively mix the properties of two objects
+            var empty = {};
+            for (var name in source) {
+                if (!(name in dest) || (dest[name] !== source[name] && (!(name in empty) || empty[name] !== source[name]))) {
+                    try {
+                        if (source[name].constructor === Object) {
+                            dest[name] = this.mixinDeep(dest[name], source[name]);
+                        } else {
+                            dest[name] = source[name];
+                        }
+                    } catch (e) {
+                        // Property in destination object not set. Create it and set its value.
+                        dest[name] = source[name];
+                    }
+                }
+            }
+            return dest;
         }
     });
 });
